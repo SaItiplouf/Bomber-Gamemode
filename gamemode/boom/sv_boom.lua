f5c9b6f836183ab39858a6280f2014741f37d8e8
Saltiplouf <polooavecdeuxo@gmail.com>
2024-03-28 00:06:49 +0100

    refact: Boom

--- gamemode/boom/sv_boom.lua
+++ gamemode/boom/sv_boom.lua
@@ -1,24 +1,25 @@
+local function createEffect(explosionPos)
+    local effectdata = EffectData()
+    effectdata:SetStart(explosionPos)
+    effectdata:SetOrigin(explosionPos)
+    effectdata:SetScale(1)
+    util.Effect("Explosion", effectdata)
+end
+
 local function CreateExplosion(ent1, ent2)
     local collisionDistance = 50
-    if ent1:GetPos():Distance(ent2:GetPos()) <= collisionDistance then
-        print("Collision entre deux joueurs: " .. ent1:Name() .. " et " .. ent2:Name())
-        local explosionPos = (ent1:GetPos() + ent2:GetPos()) / 2
-        util.ScreenShake(explosionPos, 5, 5, 1, 500)
-        ent1:EmitSound("ambient/explosions/explode_4.wav", 100, 100)
-        local effectdata = EffectData()
-        effectdata:SetStart(explosionPos)
-        effectdata:SetOrigin(explosionPos)
-        effectdata:SetScale(1)
-        util.Effect("Explosion", effectdata)
-        ent1:Kill()
-        IncrementScore(ent2)
-    end
+    if ent1:GetPos():Distance(ent2:GetPos()) > collisionDistance then return end
+    print("Collision entre deux joueurs: " .. ent1:Name() .. " et " .. ent2:Name())
+    local explosionPos = (ent1:GetPos() + ent2:GetPos()) / 2
+    util.ScreenShake(explosionPos, 5, 5, 1, 500)
+    createEffect(explosionPos)
+    ent1:EmitSound("ambient/explosions/explode_4.wav", 100, 100)
+    ent1:Kill()
+    IncrementScore(ent2)
 end
 
 function GM:ShouldCollide(ent1, ent2)
-    if ent1:IsPlayer() and ent2:IsPlayer() then
-        if not (ent1:Alive() and ent2:Alive() and ent1:Team() ~= ent2:Team()) then return false end
-        CreateExplosion(ent1, ent2)
-    end
+    if ent1:IsPlayer() and ent2:IsPlayer() and ent1:Alive() and ent2:Alive() and ent1:Team() ~= ent2:Team() then return false end
+    CreateExplosion(ent1, ent2)
     return true
 end
--- gamemode/thirdperson.lua
+++ gamemode/thirdperson.lua
@@ -1,10 +1,13 @@
+local function thirdPerson(ply, origin, angles, fov)
+    if not ply:GetNWBool("ThirdPerson", false) then return end
+    local view = {}
+    view.origin = origin - (angles:Forward() * 100) + (angles:Up() * 20)
+    view.angles = angles
+    view.fov = fov
+    view.drawviewer = true
+    return view
+end
+
 function GM:CalcView(ply, origin, angles, fov)
-    if ply:GetNWBool("Thirdperson", false) then
-        local view = {}
-        view.origin = origin - (angles:Forward() * 100) + (angles:Up() * 20)
-        view.angles = angles
-        view.fov = fov
-        view.drawviewer = true
-        return view
-    end
+    thirdPerson(ply, origin, angles, fov)
 end
