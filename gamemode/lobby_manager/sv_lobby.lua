cf90710cef1b891cf99dd32fc76a56e550887bca
Saltiplouf <polooavecdeuxo@gmail.com>
2024-03-27 23:49:37 +0100

    refact: remove useless code

--- gamemode/lobby_manager/sv_lobby.lua
+++ gamemode/lobby_manager/sv_lobby.lua
@@ -1,10 +1,8 @@
-util.AddNetworkString("start_game")
 util.AddNetworkString("client_ready")
 local function EnterLobby(ply)
     GAMEMODE:ShowTeam(ply)
 end
 
-net.Receive("start_game", function() BeginRound() end)
 net.Receive("client_ready", function(len, ply)
     EnterLobby(ply)
     ply:SetNWBool("ThirdPerson", true)
