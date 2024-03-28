d3b00d0d79670f026f6e5093b8e1e99f777d1fd1
Saltiplouf <polooavecdeuxo@gmail.com>
2024-03-28 22:41:49 +0100

    refacto: missing space between ()

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,8 +1,8 @@
-include("shared.lua")
-include("round_controller/cl_round_controller.lua")
-include("thirdperson.lua")
-include("team/cl_showteam.lua")
+include( "shared.lua" )
+include( "round_controller/cl_round_controller.lua" )
+include( "thirdperson.lua" )
+include( "team/cl_showteam.lua" )
 function GM:InitPostEntity()
-    net.Start("Monolith.Client.Ready")
+    net.Start( "Monolith.Client.Ready" )
     net.SendToServer()
 end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,10 +2,10 @@ GM.NAME = "Bomber.exe"
 GM.Prefix = "bmbexe"
 GM.Author = "ShawN"
 GM.TeamBased = true
-include("team/team.lua")
-include("player_class/player.lua")
-include("player_class/team_blue.lua")
-include("player_class/team_red.lua")
+include( "team/team.lua" )
+include( "player_class/player.lua" )
+include( "player_class/team_blue.lua" )
+include( "player_class/team_red.lua" )
 function GM:Initialize()
     self.BaseClass.Initialize( self )
 end
