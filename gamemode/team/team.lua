c8c3b5ffaaefed03bc8b514342f5b8c1df9a50b1
Saltiplouf <polooavecdeuxo@gmail.com>
2024-03-28 22:34:48 +0100

    refacto: missing braces

--- gamemode/team/team.lua
+++ gamemode/team/team.lua
@@ -2,9 +2,9 @@ TEAM_RED = 1
 TEAM_BLUE = 2
 function GM:CreateTeams()
     team.SetUp( TEAM_RED, "Red Team", Color(255, 0, 0) )
-    team.SetClass( TEAM_RED, {"player_red"} )
+    team.SetClass( TEAM_RED, { "player_red" } )
     team.SetUp( TEAM_BLUE, "Blue Team", Color(0, 0, 255) )
-    team.SetClass( TEAM_BLUE, {"player_blue"} )
+    team.SetClass( TEAM_BLUE, { "player_blue" } )
     team.SetUp( TEAM_SPECTATOR, "Spectateur", Color(150, 150, 150) )
     team.SetSpawnPoint( TEAM_SPECTATOR, "worldspawn" )
 end
